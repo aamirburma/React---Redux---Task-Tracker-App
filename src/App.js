16027d92f442f1f8e45053239553295d1b168281
Aamirsohel Burma <burmaaamir@gmail.com>
2021-12-23 22:09:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './MyComponents/Header';
+import { Footer } from './MyComponents/Footer';
+import { TaskItem } from './MyComponents/TaskItem';
+import { Tasks } from './MyComponents/Tasks';
+import React, {useState} from 'react';
+import { AddTask } from './MyComponents/AddTask';
 
 function App() {
+  const onDelete = (task) => {
+    console.log("I am on Delete",task);
+    setTasks(tasks.filter((e) => {
+      return e!==task;
+    }))
+  }
+
+  const addTask = (title,desc) => {
+    console.log("Add Task",title,desc);
+    let sno;
+    if(tasks.length === 0){
+      sno = 1
+    }else{
+      sno = tasks[tasks.length - 1].sno + 1;
+    }
+    const myTask = {
+      sno:sno,
+      title:title,
+      desc:desc
+    }
+    setTasks([...tasks,myTask]);
+    console.log(myTask);
+  }
+  const [tasks, setTasks] = useState([
+    {
+      sno :1,
+      title:"Goto School",
+      desc:"You must to goto school for lesving certificate"
+    },
+    {
+      sno :2,
+      title:"Drink water",
+      desc:"Regular water drink"
+    },
+    {
+      sno :3,
+      title:"Play soccer",
+      desc:"Time to play"
+    }
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="12" />
+      <AddTask addTask={addTask} />
+      <Tasks tasks={tasks} onDelete={onDelete} /> 
+      <Footer />
+    </>
   );
 }
 
